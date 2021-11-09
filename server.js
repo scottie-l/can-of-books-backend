c9669665be1c0b18a88f459027abe10c47fb4c1c
Scott Lease <scottie_lease@hotmail.com>
2021-11-09 15:35:06 -0800

    create book working on backend

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const mongoose = require('mongoose');
 const book = require('./book.js');
 // const getBooks = require('./bookRoute.js');
 
-
-
-
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -24,6 +22,7 @@ const PORT = process.env.PORT || 3001;
 
 async function getBooks(req, res) {
   let request = req.query;
+  console.log(req.body);
   if (request) {
       try {
           let booksFromDB = await book.find({});
@@ -40,6 +39,31 @@ async function getBooks(req, res) {
   }
 }
 
+async function createBook (req, res) {
+  console.log(req.body);
+  try {
+    let newBook = await book.create(req.body);
+    res.status(201).send(newBook);
+  } catch(e) {
+    res.status(400).send('Book not added');
+  }
+}
+
+async function deleteBook (req, res) {
+  console.log(req.query);
+  const id = req.params.id;
+  const email = req.query.email;
+  try {
+    let deleteBook = await book.find(req.params.id);
+    if (deleteBook.email === email) {
+      await book.deleteOne(req.params.id)
+    }
+    res.status(204).send('Book deleted');
+  } catch(e) {
+    res.status(400).send('Book not deleted');
+  }
+}
+
 app.get('/test', (request, response) => {
 
   response.send('test request received')
@@ -47,5 +71,8 @@ app.get('/test', (request, response) => {
 })
 
 app.get('/books', getBooks);
+
+app.post('/books', createBook);
+app.delete('/books', deleteBook);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
