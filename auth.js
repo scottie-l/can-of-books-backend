65101d81aff2013b9e2106d1bfdb433c460c7f56
Scott Lease <scottie_lease@hotmail.com>
2021-11-14 15:00:33 -0800

    finished lab14

---
+++ auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const client = jwksClient({
+    jwksUri: process.env.JWKS_URI
+});
+
+function getKey(header, callback) {
+    client.getSigningKey(header.kid, function(err, key){
+        const signingKey = key.publicKey || key.rsaPublicKey;
+        callback(null, signingKey);
+    });
+}
+
+function verifyUser(request, errOrUserCallback) {
+    try {
+        const token = request.headers.authorization.split(' ')[1];
+        console.log(token);
+        jwt.verify(token, getKey, {}, errOrUserCallback);
+    } catch(error) {
+        errOrUserCallback('not authorized');
+    }
+}
+
+module.exports = verifyUser;
